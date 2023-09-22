8de969738d3da98e9dca4e6bec25c3722ea1e414
InJun2 <os2864@naver.com>
2023-09-23 03:42:55 +0900

    - 에러 메시지, DTO 템플릿 추가

---
+++ src/main/java/com/example/deokjideokji/error/dto/ErrorResponseDto.java
@@ -0,0 +1,23 @@
+package com.example.deokjideokji.error.dto;
+
+import lombok.Data;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ErrorResponseDto {
+    private String message;
+    private final LocalDateTime serverDateTime;
+
+    public ErrorResponseDto(String message) {
+        this.message = message;
+        this.serverDateTime = LocalDateTime.now();
+    }
+
+    public static ResponseEntity<ErrorResponseDto> of(ErrorMessage message) {
+        return ResponseEntity
+                .status(message.getStatus())
+                .body(new ErrorResponseDto(message.name()));
+    }
+}
