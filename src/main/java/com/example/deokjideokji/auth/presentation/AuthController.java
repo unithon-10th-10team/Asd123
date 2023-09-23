1693e4cd004aad320b2997a82439ac2095ec579d
InJun2 <os2864@naver.com>
2023-09-23 19:03:12 +0900

    - 멤버 도메인 약 수정

--- src/main/java/com/example/deokjideokji/auth/presentation/AuthController.java
+++ src/main/java/com/example/deokjideokji/auth/presentation/AuthController.java
@@ -1,12 +1,17 @@
 package com.example.deokjideokji.auth.presentation;
 
 import com.example.deokjideokji.auth.application.AuthService;
+import com.example.deokjideokji.common.header.HeaderUtil;
 import com.example.deokjideokji.common.response.dto.ResponseDto;
 import com.example.deokjideokji.common.response.dto.ResponseMessage;
 import com.nimbusds.jose.shaded.json.parser.ParseException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,8 +27,11 @@ public class AuthController {
      * @return     * userDetail : 조회한 유저 정보를 담은 jwt token
      */
     @GetMapping("/kakao")
-    public ResponseEntity<?> login(@RequestParam String token) throws ParseException {
-        var userDetail = authService.userSignUp(token);
+    public ResponseEntity<?> login() throws ParseException {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
+                .getRequestAttributes()).getRequest();
+
+        var userDetail = authService.userSignUp(request);
 
         return ResponseDto.toResponseEntity(ResponseMessage.SUCCESS_LOAD_MEMBER_INFORMATION, userDetail);
     }
--- src/main/java/com/example/deokjideokji/member/application/MemberService.java
+++ src/main/java/com/example/deokjideokji/member/application/MemberService.java
@@ -1,5 +1,7 @@
 package com.example.deokjideokji.member.application;
 
+import com.example.deokjideokji.auth.token.TokenProvider;
+import com.example.deokjideokji.common.header.HeaderUtil;
 import com.example.deokjideokji.member.domain.Member;
 import com.example.deokjideokji.member.dto.response.MemberResponse;
 import com.example.deokjideokji.member.infrastructure.MemberRepository;
@@ -7,12 +9,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
+    private final TokenProvider tokenProvider;
 
     private final MemberRepository memberRepository;
 
@@ -21,9 +25,10 @@ public class MemberService {
      * @return  List<MemberResponse> : 회원 모든 회원 정보 List
      */
     @Transactional(readOnly = true)
-    public List<MemberResponse> searchAllMember() {
-        return memberRepository.findAll().stream()
-                .map(Member::toResponseDto)
-                .collect(Collectors.toList());
+    public MemberResponse searchAllMember(HttpServletRequest request) {
+        var token = HeaderUtil.getAccessToken(request);
+        var memberId = tokenProvider.getUserToken(token);
+
+        return memberRepository.findById(memberId).get().toResponseDto();
     }
 }
